61e16f3c5fdc46a563cf6e289e8c896327ba246f
Jakob Hellermann <jakob.hellermann@protonmail.com>
2024-06-10 16:52:20 +0200

    Initial commit

---
+++ Source/ExampleMod.cs
@@ -0,0 +1,19 @@
+﻿using BepInEx;
+using NineSolsAPI;
+
+namespace ExampleMod;
+
+[BepInDependency(NineSolsAPICore.PluginGUID)]
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+public class ExampleMod : BaseUnityPlugin {
+    private void Awake() {
+        RCGLifeCycle.DontDestroyForever(gameObject);
+        Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        
+        ToastManager.Toast("Example Toast Message");
+    }
+
+    private void OnDestroy() {
+        // Make sure to clean up resources here to enable hot reloading
+    }
+}
